90a2993e796623e6d75ae034e14d8747267e4cac
KIRUYXAN <279901@gmail.com>
2021-12-09 01:46:46 +0300

    /

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.WriteLine("Давайте вычеслим ваш ИМТ");
+Console.WriteLine("Введите ваше вес в кг:");
+double m = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите вашу рост в см: ");
+double h = Convert.ToDouble(Console.ReadLine());
+h = h / 100;
+double imt = m / (h * h);
+Console.WriteLine($"Ваш ИМТ: {imt:F2}");
