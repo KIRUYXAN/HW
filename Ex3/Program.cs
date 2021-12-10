a80cf0cb2206f4b842e9aa596559b7b4df323c1c
KIRUYXAN <279901@gmail.com>
2021-12-10 21:43:42 +0300

    .

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -1,4 +1,12 @@
-﻿Console.WriteLine("Введи x1: ");
+﻿static double stepen(double a, double atimes)
+{
+    for (int i = 1; i < atimes; i++)
+        return a * a;
+    return atimes = Convert.ToDouble("");
+}
+
+
+Console.WriteLine("Введи x1: ");
 double x1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введи x2: ");
 double x2 = Convert.ToDouble(Console.ReadLine());
@@ -8,3 +16,10 @@ Console.WriteLine("Введи y2: ");
 double y2 = Convert.ToDouble(Console.ReadLine());
 double r = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 Console.WriteLine($"{r:F2}");
+
+Console.WriteLine("");
+
+double r1 = x2 - x1;
+double r2 = y2 - y1;
+double r3 = Math.Sqrt(stepen(r1, 2) + stepen(r2, 2));
+Console.WriteLine($"{r3:F2}");
