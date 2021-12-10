3715d669efba0d4a0990e572ed7ca33eae54cc6b
KIRUYXAN <279901@gmail.com>
2021-12-10 20:40:08 +0300

    .

---
+++ Ex5/Program.cs
@@ -0,0 +1,11 @@
+﻿static void Print(string msg, int x, int y)
+{
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine(msg);
+}
+
+string n = "Кирилл";
+string sn = "Потемкин";
+string c = "Москва";
+
+Print("{sn} {n} {c}", 50, 50);
