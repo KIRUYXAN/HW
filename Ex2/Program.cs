ef6130cbf4ee36c2f624f2de09b4ddecc44661a1
KIRUYXAN <279901@gmail.com>
2021-12-12 23:48:58 +0300

    .

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -2,7 +2,9 @@
 Console.WriteLine("Введите ваше вес в кг:");
 double m = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите вашу рост в см: ");
-double h = Convert.ToDouble(Console.ReadLine());
-h = h / 100;
+double h = Convert.ToDouble(Console.ReadLine()) / 100;
+Console.WriteLine(" ");
+
 double imt = m / (h * h);
+
 Console.WriteLine($"Ваш ИМТ: {imt:F2}");
